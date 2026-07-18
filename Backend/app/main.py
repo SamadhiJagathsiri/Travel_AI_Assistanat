b3e0e6ee20abcfc310a1a29ce44225d07ac862d0
Samadhi Jagathsiri <anusarasamm@gmail.com>
2026-07-18 09:27:33 +0530

    Fix CORS for Vercel

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -24,9 +24,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)git add .
-git commit -m "Fix CORS for Vercel"
-git push
+)
 
 
 @app.get("/")
